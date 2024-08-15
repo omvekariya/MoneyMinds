38420119d111a999981d75e988be7b7250c3b4fd
Om Vekariya <omvekariya31@gmail.com>
2024-08-16 00:28:56 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] , variable: '--font-inter'});
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ["latin"], 
+  weight: ['400', '700'],
+  variable: '--font-ibm-plex-serif',
+})
+
+export const metadata: Metadata = {
+  title: "MoneyMinds",
+  description: "MoneyMinds is an advanced banking system designed to streamline financial operations through innovative solutions. It offers secure and efficient management of personal and corporate finances, including features like real-time transaction processing and user-friendly interfaces for seamless customer interactions. The system is built to support scalability, ensuring it meets the demands of modern banking while adhering to strict security standards.",
+  icons: {
+    icon: '/icons/logo.svg',
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+    </html>
+  );
+}
